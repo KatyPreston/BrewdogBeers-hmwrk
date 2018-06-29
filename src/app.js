48422fb80c01df7fc7e240106abb222fb623be46
KatyPreston <prestonkj@hotmail.com>
2018-06-29 12:26:25 +0100

    request for data from API working

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
+const Beers = require("./models/beers.js");
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log("JavaScript loaded");
-  
+
+  const beers = new Beers();
+  beers.getData();
+
 });
--- src/models/beers.js
+++ src/models/beers.js
@@ -2,7 +2,23 @@ const PubSub = require("../helpers/pub_sub.js");
 const Request = require("../helpers/request.js");
 
 const Beers = function(){
-
+  this.data = [];
 };
+
+Beers.prototype.getData = function (beer) {
+  const url = `https://api.punkapi.com/v2/beers`;
+  const request = new Request(url);
+  const handleRequestComplete = (responseData) => {
+    this.data = responseData;
+    PubSub.publish('Beers:all-beers-loaded', this.data);
+    console.log(this.data);
+  };
+
+  request.get()
+    .then(handleRequestComplete)
+    .catch(error => console.error(error));
+}
+
+
 
 module.exports = Beers;
